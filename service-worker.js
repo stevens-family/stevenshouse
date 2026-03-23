aab10c64b16d1abf8860b656f59f0a60f3c480d2
stevens-family <family@stevenshouse.org>
2026-03-23 10:58:38 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+var CACHE_NAME = 'stevens-family-v1';
+var URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './css/styles.css',
+  './js/script.js',
+  './manifest.json',
+  './images/logo.svg',
+  './images/favicon.png',
+  './images/icon-192.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(name) { return name !== CACHE_NAME; })
+          .map(function(name) { return caches.delete(name); })
+      );
+    })
+  );
+});
